342a069ef191b819153eff8e47cb46e54ef51524
ankitjain28may <ankitjain28may77@gmail.com>
2016-09-11 02:04:09 +0530

    integrated with js n jquery

--- verticalScroller.js
+++ verticalScroller.js
@@ -1,26 +1,63 @@
-var iScrollHeight = $('#AutoScroll').prop('scrollHeight');
-    var iScrollTop = $("#AutoScroll").prop("scrollTop");
-    var he = $("#AutoScroll").height();
-    console.log(iScrollHeight);
-    console.log(iScrollTop);
-    var id = setInterval(scrollBar , 2000);
-    function scrollBar() {
-        if(iScrollTop+he < iScrollHeight-20)
+ function Scroller(scrollId) {
+    this.AutoScroll = '#'+scrollId;
+    this.iScrollHeight = $(this.AutoScroll).prop('scrollHeight');
+    this.iScrollTop = $(this.AutoScroll).prop("scrollTop");
+    this.he = $(this.AutoScroll).height();
+    // console.log(this.AutoScroll);
+    // console.log(this.iScrollHeight);
+    // console.log(this.iScrollTop);
+    var self = this;
+    this.id = setInterval(function () {
+        if(self.iScrollTop+self.he < self.iScrollHeight-20)
         {
-            iScrollTop = $("#AutoScroll").prop("scrollTop");
-            iScrollTop+=100;
-            // $("#AutoScroll").prop('scrollTop',iScrollTop);
-            $("#AutoScroll").animate(
-                {scrollTop: iScrollTop},
+            // console.log(self.iScrollTop + " " + self.AutoScroll);
+            self.iScrollTop = $(self.AutoScroll).prop("scrollTop");
+            self.iScrollTop+=100;
+            // $(self.AutoScroll).prop('scrollTop',self.iScrollTop);
+            $(self.AutoScroll).animate(
+                {scrollTop: self.iScrollTop},
                 "slow",
                 "linear");
         }
         else
         {
-            iScrollTop-=200;
-            $("#AutoScroll").animate(
+            self.iScrollTop-=200;
+            $(self.AutoScroll).animate(
                 {scrollTop: "0px"},
                 "fast",
                 "swing");
         }
-    }
+    } , 2000);
+}
+
+(function( $ ){
+   $.fn.scroller = function() {
+        var element = this;
+        console.log(element.selector);
+        this.AutoScroll = element.selector;
+        this.iScrollHeight = $(this.AutoScroll).prop('scrollHeight');
+        this.iScrollTop = $(this.AutoScroll).prop("scrollTop");
+        this.he = $(this.AutoScroll).height();
+
+        var self = this;
+        this.id = setInterval(function () {
+            if(self.iScrollTop+self.he < self.iScrollHeight-20)
+            {
+                self.iScrollTop = $(self.AutoScroll).prop("scrollTop");
+                self.iScrollTop+=100;
+                $(self.AutoScroll).animate(
+                    {scrollTop: self.iScrollTop},
+                    "slow",
+                    "linear");
+            }
+            else
+            {
+                self.iScrollTop-=200;
+                $(self.AutoScroll).animate(
+                    {scrollTop: "0px"},
+                    "fast",
+                    "swing");
+            }
+        } , 2000);
+    };
+})( jQuery );
